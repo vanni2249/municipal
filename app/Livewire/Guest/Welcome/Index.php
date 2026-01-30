b2cf8d63a3d72c25aca26882d141f122c38d1c73
Geovanni Colon Barrios <vanni2249@gmail.com>
2026-01-29 23:04:49 -0400

    feat: implement service retrieval in welcome component; update view to display services with details

--- app/Livewire/Guest/Welcome/Index.php
+++ app/Livewire/Guest/Welcome/Index.php
@@ -2,18 +2,23 @@
 
 namespace App\Livewire\Guest\Welcome;
 
+use App\Models\Service;
 use App\Models\Type;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
 
 class Index extends Component
 {
+    public $services;
+
+    public function mount()
+    {
+        $this->services = Service::get();
+    }
 
     #[Layout('layouts.landing')]
     public function render()
     {
-        return view('livewire.guest.welcome.index', [
-            // 'types' => Type::with(['services'])->get()
-        ]);
+        return view('livewire.guest.welcome.index');
     }
 }
--- resources/views/livewire/guest/welcome/index.blade.php
+++ resources/views/livewire/guest/welcome/index.blade.php
@@ -1,5 +1,5 @@
 <div>
-   <section class="bg-black h-[30rem] border-0">
+    <section class="bg-black h-[30rem] border-0">
         <div class="max-w-7xl px-4 mx-auto flex flex-col items-center justify-center h-full">
             <div class="grow flex flex-col justify-center items-center h-full">
                 <h1
@@ -18,8 +18,7 @@
                     Este sitio web es un esfuerzo por parte del gobierno municipal de la ciudad de San Antonio para
                     brindar a los ciudadanos acceso a información y servicios en línea.
                 </p>
-                <ul
-                    class="flex md:hidden items-center flex-row space-x-1 ">
+                <ul class="flex md:hidden items-center flex-row space-x-1 ">
                     {{-- <li>
                         <a href="{{ route('users.login') }}"
                             class="bg-gray-700 hover:bg-gray-800 border-gray-400 text-gray-200 hover:text-white px-6 py-2 text-xs uppercase font-bold rounded-full">Iniciar Sesión</a>
@@ -32,7 +31,7 @@
             </div>
         </div>
     </section>
-    <section class="max-w-7xl px-4 mx-auto pt-4">
+    {{-- <section class="max-w-7xl px-4 mx-auto pt-4">
         <x-card>
             <div class="flex flex-col md:flex-row justify-between items-center gap-4">
                 <div class="grow">
@@ -44,87 +43,39 @@
                     </p>
                 </div>
                 <div class="w-full md:w-auto flex ">
-                    {{-- <x-link-button href="{{ route('users.attach') }}" class="w-full whitespace-nowrap" label="Adjuntar numero de registro" /> --}}
                 </div>
             </div>
         </x-card>
-    </section>
+    </section> --}}
     <section class="grid grid-cols-12 gap-2 lg:gap-4 pt-4  max-w-7xl px-4 mx-auto">
         <header id="services" class="col-span-full pt-4 px-2">
             <h2 class="font-bold text-xl text-gray-800">
                 Servicios municipales
             </h2>
         </header>
-        <!-- Citizens services -->
-        {{-- @php
-            $services = [
-                [
-                    'key' => 'citizen',
-                    'name' => 'Servicios al ciudadano',
-                    'query' => App\Data\Services\User::items(),
-                    'route' => 'users.login',
-                ],
-                [
-                    'key' => 'merchant',
-                    'name' => 'Servicios al comerciante',
-                    'query' => App\Data\Services\Merchant::items(),
-                    'route' => 'users.login',
-                    
-                ],
-                [
-                    'key' => 'accountant',
-                    'name' => 'Servicios al contador',
-                    'query' => App\Data\Services\Accountant::items(),
-                    'route' => 'users.login',
-                    
-                ],
-                [   
-                    'key' => 'contractor',
-                    'name' => 'Servicios al contratista',
-                    'query' => App\Data\Services\Contractor::items(),
-                    'route' => 'users.login',
-                    
-                ],
-                [
-                    'key' => 'supplier',
-                    'name' => 'Servicios al Supplidor',
-                    'query' => App\Data\Services\Supplier::items(),
-                    'route' => 'users.login',
-                    
-                ],
-                [
-                    'key' => 'visitors',
-                    'name' => 'Servicios al visitante',
-                    'query' => App\Data\Services\Visitor::items(),
-                    'route' => 'users.login',
-                ],
-            ];
-        @endphp      --}}
-        {{-- @foreach ($services as $service)
-        <div class="bg-white col-span-full md:col-span-6 lg:col-span-4 px-4 py-6 md:p-6 lg:p-8 rounded-xl flex flex-col space-y-6">
-            <header class="flex items-center justify-between">
-                <h2 class="text-lg font-bold text-gray-700 leading-3">
-                    {{ $service['name'] }}
-                </h2>
-            </header>
-            @if ($service['key'] != 'employee')
-                
-            <ul class="grow text-sm space-y-4 py-2 px-1">
-                @foreach (collect($service['query'])->take(6) as $item)
-                <li class="text-gray-600 line-clamp-1 ">
-                    {{ $item['title'] }}
-                </li>
-                @endforeach
-            </ul>
-            @endif
-            <footer class="flex justify-center">
-                <a href="{{ route($service['route'], ['role' => $service['key']]) }}"
-                    class="border border-gray-300 font-bold text-gray-600 hover:text-gray-800 transition-all hover:bg-gray-200 w-full text-center text-xs py-2 rounded-full">
-                    Acceder a los servicios
-                </a>
-            </footer>
-        </div>
-        @endforeach --}}
-    </section>
+        <div class="col-span-12 grid grid-cols-12 gap-2 md:gap-4">
+        @foreach ($services as $service)
+            <x-card class="col-span-full md:col-span-6 lg:col-span-4 flex flex-col">
+                <div>
+                    <small class="text-gray-700">
+                        {{ $service->serviceType->name }}
+                    </small>
+                    <br>
+                    <strong class="text-md">{{ $service->title }}</strong>
+                </div>
+                <p class="text-sm text-gray-600 line-clamp-2 grow mb-4">
+                    {{ $service->description }}
+                </p>
+                {{-- <div class="flex justify-between items-center mt-auto">
+                    <div class="text-sm text-gray-800">
+                       <x-money-format :amount="$service->amount" />
+                    </div>
+                    <div class="flex justify-end">
+                        <x-link-button href="{{ route('businesses.services.show', $service->ulid) }}" variant="light">Aplicar</x-link-button>
+                    </div>
+                </div> --}}
+            </x-card>
+        @endforeach
+    </div>
     </section>
 </div>

9c79a21a5e2c346baff77be60d52e03de900d75e
Geovanni Colon <vanni2249@gmail.com>
2026-01-30 23:36:20 -0400

    refactor: simplify accounts rendering by removing inline account list and using Livewire component

--- app/Livewire/Users/Accounts/Index.php
+++ app/Livewire/Users/Accounts/Index.php
@@ -10,8 +10,6 @@ class Index extends Component
     #[Layout('layouts.user')]
     public function render()
     {
-        return view('livewire.users.accounts.index', [
-            'accounts' => auth()->user()->accounts()->with('accountType')->orderBy('account_type_id', 'asc')->get(),
-        ]);
+        return view('livewire.users.accounts.index');
     }
 }

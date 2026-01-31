a4b1b29f16566ceddaed3db3f0f683d330d32e45
Geovanni Colon <vanni2249@gmail.com>
2026-01-31 09:47:33 -0400

    feat: update hover background color for application card elements

--- resources/views/livewire/businesses/applications/index.blade.php
+++ resources/views/livewire/businesses/applications/index.blade.php
@@ -9,7 +9,7 @@
         <x-card-elements-group>
             @forelse ($applications as $application)
                 <a href="{{ route('businesses.applications.show', $application->ulid) }}" class="block" wire:navigate>
-                    <x-card-element class="hover:bg-gray-50" border="{{ $application->status->statusType->variant }}">
+                    <x-card-element class="hover:bg-gray-200" border="{{ $application->status->statusType->variant }}">
                         <div class="flex justify-between items-start space-x-2">
                             <div class="flex-1 flex flex-col space-y-1">
                                 <span class="text-gray-700 font-bold uppercase text-xs">

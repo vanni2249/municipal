508025bcec7ecaebee72135e8a15ac199d5f8e4b
Geovanni Colon <vanni2249@gmail.com>
2026-01-31 09:22:28 -0400

    feat: add home icon to icon component and adjust size handling for SVGs

--- resources/views/components/icon.blade.php
+++ resources/views/components/icon.blade.php
@@ -1,8 +1,10 @@
-@props(['variant' => 'primary', 'icon' => 'edit' , 'width' => 24, 'height' => 24])
+@props(['variant' => 'primary', 'icon' => 'edit' , 'width' => 24, 'height' => 24, 'size' => 6])
 
 <div class="">
 
     @switch($icon)
+
+    {{-- Tabler icons --}}
         @case('minus')
             <svg xmlns="http://www.w3.org/2000/svg" width="{{ $width }}" height="{{ $height }}" viewBox="0 0 24 24" fill="none"
                 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
@@ -73,37 +75,6 @@
             </svg>
         @break
 
-        @case('arrow-right')
-            <svg xmlns="http://www.w3.org/2000/svg" width="{{ $width }}" height="{{ $height }}" viewBox="0 0 16 16" fill="currentColor" class="size-4">
-                <path fill-rule="evenodd"
-                    d="M2 8a.75.75 0 0 1 .75-.75h8.69L8.22 4.03a.75.75 0 0 1 1.06-1.06l4.5 4.5a.75.75 0 0 1 0 1.06l-4.5 4.5a.75.75 0 0 1-1.06-1.06l3.22-3.22H2.75A.75.75 0 0 1 2 8Z"
-                    clip-rule="evenodd" />
-            </svg>
-        @break
-
-        @case('bars-3')
-            <svg xmlns="http://www.w3.org/2000/svg" width="{{ $width }}" height="{{ $height }}" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                class="size-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-            </svg>
-        @break
-
-        @case('bell')
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                stroke="currentColor" class="size-6">
-                <path stroke-linecap="round" stroke-linejoin="round"
-                    d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
-            </svg>
-        @break
-
-        @case('user-circle')
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                stroke="currentColor" class="size-6">
-                <path stroke-linecap="round" stroke-linejoin="round"
-                    d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-            </svg>
-        @break
-
         @case('home')
             <svg xmlns="http://www.w3.org/2000/svg" width="{{ $width }}" height="{{ $height }}" viewBox="0 0 24 24" fill="none"
                 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
@@ -114,6 +85,40 @@
                 <path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6" />
             </svg>
         @break
+
+        {{-- Heroicons --}}
+
+        @case('arrow-right')
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-{{ $size }}">
+                <path fill-rule="evenodd"
+                    d="M2 8a.75.75 0 0 1 .75-.75h8.69L8.22 4.03a.75.75 0 0 1 1.06-1.06l4.5 4.5a.75.75 0 0 1 0 1.06l-4.5 4.5a.75.75 0 0 1-1.06-1.06l3.22-3.22H2.75A.75.75 0 0 1 2 8Z"
+                    clip-rule="evenodd" />
+            </svg>
+        @break
+
+        @case('bars-3')
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            </svg>
+        @break
+
+        @case('bell')
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor" class="size-{{ $size }}">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
+            </svg>
+        @break
+
+        @case('user-circle')
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor" class="size-{{ $size }}">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+            </svg>
+        @break
+
 
         @default
     @endswitch
--- resources/views/livewire/users/accounts/components/account-list.blade.php
+++ resources/views/livewire/users/accounts/components/account-list.blade.php
@@ -2,33 +2,32 @@
     <x-card>
         <x-card-elements-group>
             @forelse ($accounts as $account)
-                <x-card-element class="flex justify-between items-center" border="secondary">
-                    <div>
-                        <strong class="text-sm">{{ $account->accountType->name }}</strong>
-                        <br>
-                        <span class="text-gray-700 text-sm">{{ $account->number }}</span>
-                    </div>
                     @switch($account->accountType->slug)
-                        @case('citizen')
-                            <x-icon-link href="{{ route('citizens.set-session', ['account' => $account->ulid]) }}"
-                                icon="arrow-right" variant="primary-outline" size="xs" wire:navigate />
+                         @case('citizen')
+                            <a href="{{ route('citizens.set-session', ['account' => $account->ulid]) }}" class="block" wire:navigate>
                         @break
 
                         @case('merchant')
-                            <x-icon-link
-                                href="{{ route('users.accounts.businesses.index', ['account' => $account->ulid]) }}"
-                                icon="arrow-right" variant="primary-outline" size="xs" wire:navigate />
+                            <a href="{{ route('users.accounts.businesses.index', ['account' => $account->ulid]) }}" class="block" wire:navigate>    
                         @break
 
                         @case('accountant')
-                            <x-icon-link
-                                href="{{ route('users.accounts.merges.index', ['account' => $account->ulid]) }}"
-                                icon="arrow-right" variant="primary-outline" size="xs" wire:navigate />
+                            <a href="{{ route('users.accounts.merges.index', ['account' => $account->ulid]) }}" class="block" wire:navigate>
                         @break
 
                         @default
                     @endswitch
-                </x-card-element>
+                    <x-card-element class="flex justify-between items-center hover:bg-gray-200" border="secondary">
+                        <div>
+                            <strong class="text-sm">{{ $account->accountType->name }}</strong>
+                            <br>
+                            <span class="text-gray-700 text-sm">{{ $account->number }}</span>
+                        </div>
+                        <div>
+                            <x-icon icon="arrow-right" size="5" class="text-gray-400" />
+                        </div>
+                    </x-card-element>
+                </a>
                 @empty
                     <x-card-element>
                         <p class="text-gray-700 text-sm">No tienes cuentas asociadas a tu usuario.</p>
@@ -37,4 +36,4 @@
             </x-card-elements-group>
         </x-card>
     </div>
-</div>
+    </div>

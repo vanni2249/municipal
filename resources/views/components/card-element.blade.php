60d98919d57013d26c6c8599db4ce7fbf8665f2b
Geovanni Colon Barrios <vanni2249@gmail.com>
2026-01-30 21:30:03 -0400

    fix: update badge component colors for consistency; refactor card-element to support dynamic border styles

--- resources/views/components/card-element.blade.php
+++ resources/views/components/card-element.blade.php
@@ -1,3 +1,19 @@
-<div {{ $attributes->merge(['class' => 'bg-gray-100 p-2 lg:p-4 rounded-md']) }}>
+@props(['border' => 'none'])
+
+    @php
+        $borders = [
+            'primary' => ' border-l-4 border-gray-300 ',
+            'secondary' => ' border-l-4 border-gray-400 ',
+            'success' => ' border-l-4 border-green-300 ',
+            'danger' => ' border-l-4 border-red-500 ',
+            'warning' => ' border-l-4 border-yellow-400 ',
+            'info' => ' border-l-4 border-blue-500 ',
+            'light' => ' border-l-4 border-gray-200 ',
+            'dark' => ' border-l-4 border-gray-800 ',
+            'none' => ' border-0 ',
+        ];
+    @endphp
+
+<div {{ $attributes->merge(['class' => ' ' . $borders[$border] . ' bg-gray-100 p-2 lg:p-4 rounded-md']) }}>
     {{ $slot }}
 </div>

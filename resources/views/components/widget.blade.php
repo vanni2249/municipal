c91218acc9e947de7f05883c76a12a74296fb4b6
Geovanni Colon <vanni2249@gmail.com>
2026-02-02 21:25:48 -0400

    feat: refactor widget component to use variant-based styling and improve layout

--- resources/views/components/widget.blade.php
+++ resources/views/components/widget.blade.php
@@ -1,20 +1,34 @@
-@props(['color' => 'bg-white', 'title' => '', 'subtitle' => '', 'value' => '', 'border' => 'border-gray-300', 'icon' =>
-'circle', 'lineColor' => 'gray'])
-<div {{ $attributes->merge(['class' => $color . ' ' . $border . ' p-2 md:p-4 rounded border-l-2 border-'.$lineColor.'-300 flex items-center
-    justify-between']) }}>
+@props([
+    'variant' => 'secondary',
+    'title' => '',
+    'subtitle' => '',
+    'value' => '',
+])
+
+@php
+    $variants = [
+        'primary' => ' border-blue-400',
+        'secondary' => ' border-gray-400',
+        'success' => ' border-green-400',
+        'danger' => ' border-red-400',
+        'warning' => ' border-yellow-400',
+        'info' => ' border-teal-400',
+        'light' => ' border-gray-200',
+        'dark' => ' border-gray-800',
+    ]
+@endphp
+
+<div
+    {{ $attributes->merge(['class' => $variants[$variant] . ' border-l-4 bg-white p-2 md:p-4 rounded-xl ']) }}>
     <div>
         <header class="flex justify-between items-center">
             <h2 class="text-xs text-gray-600 leading-3 font-bold uppercase">
                 {{ $title }}
             </h2>
-            <div>
+            <span class="text-xs text-gray-500 leading-3 font-medium">
                 {{ $subtitle }}
-            </div>
+            </span>
         </header>
         <div class="text-xl text-gray-800 font-bold mt-2">{{ $value }}</div>
-    </div>
-
-    <div>
-        <img src="{{ asset('icons/'.$icon.'.svg') }}" class="w-8" alt="">
     </div>
 </div>

390bc7a0d2911f953a328bd9fe08429907cc7b52
Geovanni Colon Barrios <vanni2249@gmail.com>
2026-02-04 17:23:30 -0400

    fix: enhance checkbox component styling for forced color accessibility

--- resources/views/components/checkbox.blade.php
+++ resources/views/components/checkbox.blade.php
@@ -1,6 +1,6 @@
 @props(['name' => null, 'value' => null, 'checked' => false])
 <input type="checkbox" 
-{{ $attributes->merge(['class' => 'appearance-none  rounded border-gray-300 text-blue-600 shadow-sm focus:ring-blue-500']) }} 
+{{ $attributes->merge(['class' => 'appearance-none forced-colors:appearance-auto rounded border-gray-300 text-blue-600 shadow-sm focus:ring-blue-500']) }} 
 {{ $attributes->except('class') }} 
 {{ $attributes->get('checked') ? 'checked' : '' }}
 >

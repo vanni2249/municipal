12268b9417967bc1060092c8a5426f61e36b291c
Geovanni Colon Barrios <vanni2249@gmail.com>
2026-02-04 17:15:14 -0400

    fix: update date input implementation in citizen property use form for better styling

--- resources/views/livewire/citizens/applications/app-citizen-property-use/create.blade.php
+++ resources/views/livewire/citizens/applications/app-citizen-property-use/create.blade.php
@@ -19,7 +19,8 @@
                 <!-- Selecte date -->
                 <x-form-element class="col-span-full md:col-span-6 md:col-start-1 lg:col-span-3 lg:col-start-1">
                     <x-label for="date_at" value="Fecha" />
-                    <x-input id="date_at" type="date" wire:model="date_at" @class(['w-full', 'border-red-500 appearance-none' => $errors->has('date_at')]) />
+                    {{-- <x-input id="date_at" type="date" wire:model="date_at" @class(['w-full', 'border-red-500 appearance-none' => $errors->has('date_at')]) /> --}}
+                        <input type="date" name="" id="" class="w-full border border-gray-200 rounded-xl">
                     @error('date_at')
                         <x-error message="{{ $message }}" />
                     @enderror

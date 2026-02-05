d0bbb7fd9ca53cf38fa6713f738c05adf536239d
Geovanni Colon Barrios <vanni2249@gmail.com>
2026-02-04 21:58:21 -0400

    fix: enhance user profile view with additional information and improved layout

--- app/Livewire/Users/Profile/Index.php
+++ app/Livewire/Users/Profile/Index.php
@@ -3,19 +3,28 @@
 namespace App\Livewire\Users\Profile;
 
 use Livewire\Attributes\Layout;
+use Livewire\Attributes\Lazy;
 use Livewire\Component;
 
+#[Lazy()]
 class Index extends Component
 {
-    public $user;
 
     public function mount()
     {
-        $this->user = auth()->user()->with('status.statusType')->find(auth()->id());
+        sleep(1);
     }
+
+    public function placeholder()
+    {
+        return view('placeholders.views.users.profile-skeleton');
+    }
+
     #[Layout('layouts.user')]
     public function render()
     {
-        return view('livewire.users.profile.index');
+        return view('livewire.users.profile.index', [
+            'user' => auth()->user()->with('status.statusType')->find(auth()->id()),
+        ]);
     }
 }
--- resources/views/livewire/users/profile/index.blade.php
+++ resources/views/livewire/users/profile/index.blade.php
@@ -1,4 +1,4 @@
-<div>
+<div class="space-y-4">
     <x-card>
         <header class="flex justify-between items-start">
             <div>
@@ -13,5 +13,44 @@
                 </span>
             </div>
         </header>
+    </x-card>
+    <x-card class="space-y-4">
+        <header class="flex flex-row justify-between items-start">
+            <div>
+                <x-h3 value="Info del usuario" />
+                <p class="text-sm text-gray-700">
+                    Última actualización: {{ $user->updated_at->diffForHumans() }}
+                </p>
+            </div>
+            <div></div>
+        </header>
+        <ul class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            <li>
+                <x-app-element-label label="Nombre" />
+                <x-app-element-value value="{{ $user->name }}" />
+            </li>
+            <li>
+                <x-app-element-label label="Apellido" />
+                <x-app-element-value value="{{ $user->lastname }}" />
+            </li>
+            <li>
+                <x-app-element-label label="Correo electrónico" />
+                <x-app-element-value value="{{ $user->email }}" />
+            </li>
+            <li>
+                <x-app-element-label label="Número de teléfono" />
+                <x-app-element-value value="{{ $user->phone??'...' }}" />
+            </li>
+            <li>
+                <x-app-element-label label="Número de usuario" />
+                <x-app-element-value value="{{ $user->number??'...' }}" />
+            </li>
+            <li>
+                <x-app-element-label label="Fecha de nacimiento" />
+                <x-app-element-value>
+                    <x-date-format date="{{ $user->date_birth }}" format="d/m/Y" />
+                </x-app-element-value>
+            </li>
+        </ul>
     </x-card>
 </div>

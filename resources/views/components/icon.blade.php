89b5565a5fb4df13e9498781f91ee9bd813dfa52
Geovanni Colon Barrios <vanni2249@gmail.com>
2026-02-05 23:18:34 -0400

    fix: add chevron-down icon to the icon component

--- resources/views/components/icon.blade.php
+++ resources/views/components/icon.blade.php
@@ -119,6 +119,15 @@
             </svg>
         @break
 
+        @case('chevron-down')
+            <svg xmlns="http://www.w3.org/2000/svg" width="{{ $width }}" height="{{ $height }}"
+                viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-chevron-down">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M6 9l6 6l6 -6" />
+            </svg>
+        @break
+
         {{-- Heroicons --}}
         @case('arrow-right')
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"

964196e666ee73e2b8148a980ab362da0f024ec6
Geovanni Colon Barrios <vanni2249@gmail.com>
2026-02-05 23:17:02 -0400

    fix: add skeleton placeholders for application and services views

--- resources/views/placeholders/views/citizens/services-index.blade.php
+++ resources/views/placeholders/views/citizens/services-index.blade.php
@@ -5,15 +5,12 @@
                 <div class="h-4 bg-gray-300 rounded w-24"></div>
                 <div class="h-4 bg-gray-300 rounded w-48"></div>
             </div>
-            <div class="space-y-2">
-                <div class="h-8 bg-gray-300 rounded w-12"></div>
-            </div>
         </header>
     </x-card>
     <x-card>
-        <div class="grid grid-cols-1 gap-4">
-            @for ($i = 0; $i < 2; $i++)
-                <div class="h-12 bg-gray-300 rounded w-full"></div>
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            @for ($i = 0; $i < 6; $i++)
+                <div class="h-24 bg-gray-300 rounded w-full"></div>
             @endfor
         </div>
     </x-card>

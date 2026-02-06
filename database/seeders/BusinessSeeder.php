325a2bdd236191c29873002b0e97ce830a2c5faf
Geovanni Colon Barrios <vanni2249@gmail.com>
2026-02-05 23:18:28 -0400

    fix: comment out business creation and related seeding for testing purposes

--- database/seeders/BusinessSeeder.php
+++ database/seeders/BusinessSeeder.php
@@ -20,50 +20,50 @@ class BusinessSeeder extends Seeder
     {
 
         // Business 1 Giovanni
-        $business = Business::create([
-            'ulid' => $this->createBusinessUlid(),
-            'number' => $this->createBusinessNumber(),
-            'business_type_id' => 1,
-            'name' => 'Acme Corporation',
-            'account_id' => 4,
-        ]);
+        // $business = Business::create([
+        //     'ulid' => $this->createBusinessUlid(),
+        //     'number' => $this->createBusinessNumber(),
+        //     'business_type_id' => 1,
+        //     'name' => 'Acme Corporation',
+        //     'account_id' => 4,
+        // ]);
 
-        $business->addresses()->create([
-            'name' => 'Headquarters',
-            'place_id' => 1,
-            'address' => '123 Main St',
-            'postal_code' => '12345',
-        ]);
+        // $business->addresses()->create([
+        //     'name' => 'Headquarters',
+        //     'place_id' => 1,
+        //     'address' => '123 Main St',
+        //     'postal_code' => '12345',
+        // ]);
 
-        $business->statuses()->create([
-            'status_type_id' => 2,
-            'reason' => 'Initial status for business',
-        ]);
-        $business->defaults()->create([
-            'user_id' => 1,
-        ]);
+        // $business->statuses()->create([
+        //     'status_type_id' => 2,
+        //     'reason' => 'Initial status for business',
+        // ]);
+        // $business->defaults()->create([
+        //     'user_id' => 1,
+        // ]);
 
 
         // Business 2 Giovanni
-        $business = Business::create([
-            'ulid' => $this->createBusinessUlid(),
-            'number' => $this->createBusinessNumber(),
-            'business_type_id' => 1,
-            'name' => 'Second Business',
-            'account_id' => 4,
-        ]);
+        // $business = Business::create([
+        //     'ulid' => $this->createBusinessUlid(),
+        //     'number' => $this->createBusinessNumber(),
+        //     'business_type_id' => 1,
+        //     'name' => 'Second Business',
+        //     'account_id' => 4,
+        // ]);
 
-        $business->addresses()->create([
-            'name' => 'Main Office',
-            'place_id' => 2,
-            'address' => '456 Another St',
-            'postal_code' => '67890',
-        ]);
+        // $business->addresses()->create([
+        //     'name' => 'Main Office',
+        //     'place_id' => 2,
+        //     'address' => '456 Another St',
+        //     'postal_code' => '67890',
+        // ]);
 
-        $business->statuses()->create([
-            'status_type_id' => 2,
-            'reason' => 'Initial status for second business',
-        ]);
+        // $business->statuses()->create([
+        //     'status_type_id' => 2,
+        //     'reason' => 'Initial status for second business',
+        // ]);
 
         // Business 3 Angel F
         $business = Business::create([

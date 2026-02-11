462b130ab17d2e2c49c7a6ddca3d8a080f62c8f5
Geovanni Colon <vanni2249@gmail.com>
2026-02-11 08:14:29 -0400

    fix: updating welcome page adding background image

--- resources/views/livewire/guest/welcome/index.blade.php
+++ resources/views/livewire/guest/welcome/index.blade.php
@@ -1,20 +1,14 @@
 <div>
     <section style="background-image: url('{{ asset('img/app/morro.webp') }}');"
-        class="h-[30rem] border-0 bg-cover bg-center relative bg-transparent">
+        class="h-[30rem] flex flex-col border-0 bg-cover bg-center relative bg-transparent">
         {{-- Put image background --}}
         <nav @class(['w-full', ' ' => request()->routeIs('welcome')])>
             <div class="max-w-7xl mx-auto p-4">
                 <div @class([
                     'flex justify-between items-center bg-white/10 backdrop-blur-md rounded-2xl p-4',
-                    'border border-white/20'
-                    // '' => request()->routeIs('welcome'),
-                    // '' => !request()->routeIs('welcome'),
+                    'border border-white/20',
                 ]) class="">
-                    <a href="/" @class([
-                        'text-xl font-semibold text-gray-200',
-                        // 'text-gray-200' => request()->routeIs('welcome'),
-                        // '' => !request()->routeIs('welcome'),
-                    ]) wire:navigate>
+                    <a href="/" @class(['text-xl font-semibold text-gray-200']) wire:navigate>
                         MyApp's
                     </a>
                     <div class="flex lg:hidden text-white">
@@ -58,8 +52,7 @@
                 </div>
             </div>
         </nav>
-        {{-- <img src="{{ asset('img/app/morro.webp') }}" alt="Puerto Rico" class="absolute inset-0 w-full h-full object-cover opacity-30"> --}}
-        <div class="max-w-7xl px-4 mx-auto flex flex-col items-center justify-center h-full">
+        <div class="grow mb-4 max-w-7xl px-4 mx-auto flex flex-col items-center justify-center h-auto">
             <div class="grow flex flex-col justify-center items-center h-full">
                 <h1
                     class="text-[1.2rem] md:text-4xl text-center font-normal tracking-tight text-white flex flex-col space-y-1 lg:space-y-4">
@@ -74,38 +67,12 @@
                     </span>
                 </h1>
                 <p class="text-gray-100 text-sm text-center max-w-2xl py-4">
-                    Este sitio web es un esfuerzo por parte del gobierno municipal de la ciudad de San Antonio para
+                    Este sitio web es un esfuerzo por parte del gobierno municipal de la ciudad de San Jose para
                     brindar a los ciudadanos acceso a información y servicios en línea.
                 </p>
-                <ul class="flex md:hidden items-center flex-row space-x-1 ">
-                    {{-- <li>
-                        <a href="{{ route('users.login') }}"
-                            class="bg-gray-700 hover:bg-gray-800 border-gray-400 text-gray-200 hover:text-white px-6 py-2 text-xs uppercase font-bold rounded-full">Iniciar Sesión</a>
-                    </li>
-                    <li>
-                        <a href="{{ route('users.register') }}"
-                            class="border border-gray-700 hover:border-gray-700 hover:bg-gray-800 text-gray-300 hover:text-gray-100 px-6 py-2 text-xs uppercase font-bold rounded-full">Registrarse</a>
-                    </li> --}}
-                </ul>
             </div>
         </div>
     </section>
-    {{-- <section class="max-w-7xl px-4 mx-auto pt-4">
-        <x-card>
-            <div class="flex flex-col md:flex-row justify-between items-center gap-4">
-                <div class="grow">
-                    <h2 class="text-gray-800 text-lg font-bold">
-                        Tienes numero de registro?
-                    </h2>
-                    <p class="text-gray-600 text-sm mt-1">
-                        Si ya tienes un número de registro, puedes adjuntar el numero de registro con una cuenta de usuario nueva.
-                    </p>
-                </div>
-                <div class="w-full md:w-auto flex ">
-                </div>
-            </div>
-        </x-card>
-    </section> --}}
     <section class="grid grid-cols-12 gap-2 lg:gap-4 pt-4  max-w-7xl px-4 mx-auto">
         <header id="services" class="col-span-full pt-4 px-2">
             <h2 class="font-bold text-xl text-gray-800">

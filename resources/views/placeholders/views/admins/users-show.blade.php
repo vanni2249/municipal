bd97b576748b74950f680652f5d06e54e6e8124e
Geovanni Colon <vanni2249@gmail.com>
2026-02-11 08:12:07 -0400

    feat: adding placeholders view

--- resources/views/placeholders/views/admins/users-show.blade.php
+++ resources/views/placeholders/views/admins/users-show.blade.php
@@ -1,26 +1,64 @@
-<div class="animate-pulse grid grid-cols-12 gap-4">
-    <x-card class="col-span-full">
-        @include('placeholders.components.header-tertiary')
-    </x-card>
-    <div class="col-span-full lg:col-span-5 space-y-4">
-        <x-card class="">
-            @include('placeholders.components.card-header')
-            <div class="h-24 bg-gray-300 rounded w-full"></div>
+<div class="animate-pulse space-y-4">
+    <header>
+        <x-card>
+            <header class="flex justify-between space-x-2 items-start">
+                <div class="space-y-2">
+                    <div class="h-4 bg-gray-300 rounded w-56"></div>
+                    <div class="h-4 bg-gray-300 rounded w-24"></div>
+                </div>
+                <div class="h-4 bg-gray-300 rounded w-24"></div>
+            </header>
         </x-card>
-        <x-card class="">
-            @include('placeholders.components.card-header')
-            @include('placeholders.components.card-elements-horizontal')
-        </x-card>
-        <x-card class="">
-            @include('placeholders.components.card-header')
-            @include('placeholders.components.card-elements-horizontal')
-        </x-card>
-    </div>
-    <div class="col-span-full lg:col-span-7">
-        <x-card class="">
-            @include('placeholders.components.card-header')
-            @include('placeholders.components.card-elements-horizontal')
-        </x-card>
+    </header>
+    <div class="grid grid-cols-12 gap-4">
+        <div class="col-span-full lg:col-span-5 space-y-4   ">
+            <x-card>
+                <header class="flex justify-between space-x-2 items-start">
+                    <div class="h-4 bg-gray-300 rounded w-48"></div>
+                </header>
+                <div class="grid grid-cols-1 lg:grid-cols-2 gap-4">
+                    @for ($i = 0; $i < 2; $i++)
+                        <div class="space-y-2">
+                            <div class="h-4 bg-gray-300 rounded w-24"></div>
+                            <div class="h-10 bg-gray-300 rounded w-full"></div>
+                        </div>
+                    @endfor
+                    <div class="col-span-full lg:col-span-2 space-y-2">
+                        <div class="h-4 bg-gray-300 rounded w-24"></div>
+                        <div class="h-10 bg-gray-300 rounded w-full"></div>
+                    </div>
+                </div>
+            </x-card>
+            <x-card>
+                <header class="flex justify-between space-x-2 items-start">
+                    <div class="h-4 bg-gray-300 rounded w-48"></div>
+                </header>
+                <div class="grid grid-cols-1 gap-4">
+                    @for ($i = 0; $i < 2; $i++)
+                        <div class="space-y-2">
 
+                            {{-- <div class="h-4 bg-gray-300 rounded w-24"></div> --}}
+                            <div class="h-10 bg-gray-300 rounded w-full"></div>
+                        </div>
+                    @endfor
+                </div>
+            </x-card>
+        </div>
+        <div class="col-span-full lg:col-span-7">
+            <x-card>
+                <header class="flex justify-between space-x-2 items-start">
+                    <div class="h-4 bg-gray-300 rounded w-48"></div>
+                </header>
+                <div class="grid grid-cols-1 gap-4">
+                    @for ($i = 0; $i < 2; $i++)
+                        <div class="space-y-2">
+
+                            <div class="h-4 bg-gray-300 rounded w-24"></div>
+                            <div class="h-10 bg-gray-300 rounded w-full"></div>
+                        </div>
+                    @endfor
+                </div>
+            </x-card>
+        </div>
     </div>
 </div>

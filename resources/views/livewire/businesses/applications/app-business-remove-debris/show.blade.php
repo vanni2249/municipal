c9f0a569821ac550df60873f2b0523646af86817
Geovanni Colon Barrios <vanni2249@gmail.com>
2026-02-12 15:53:38 -0400

    feat: update layout for business applications with card component and additional details

--- resources/views/livewire/businesses/applications/app-business-remove-debris/show.blade.php
+++ resources/views/livewire/businesses/applications/app-business-remove-debris/show.blade.php
@@ -1,15 +1,26 @@
 <div>
-    <header>
-        <x-h3>Detalles de recogido de basura</x-h3>
-    </header>
-    <x-app-elements>
-        <x-app-element class="col-span-full md:col-span-4">
-            <x-app-element-label label="Dirección" />
-            <x-app-element-value value="{{ $application->business->address->address }}" />
-        </x-app-element>
-        <x-app-element class="col-span-full">
-            <x-app-element-label label="Descripción" />
-            <x-app-element-value value="{{ $application->applicable->description }}" />
-        </x-app-element>
-    </x-app-elements>
+    <x-card>
+
+        <header>
+            <x-h3>Detalles de recogido de basura</x-h3>
+        </header>
+        <x-app-elements>
+            <x-app-element class="col-span-full">
+                <x-app-element-label label="Dirección" />
+                <x-app-element-value value="{{ $application->business->address->address }}" />
+            </x-app-element>
+            <x-app-element class="col-span-3">
+                <x-app-element-label label="Lugar" />
+                <x-app-element-value value="{{ $application->business->address->place->name }}" />
+            </x-app-element>
+            <x-app-element class="col-span-3">
+                <x-app-element-label label="Código Postal" />
+                <x-app-element-value value="{{ $application->business->address->postal_code }}" />
+            </x-app-element>
+            <x-app-element class="col-span-full">
+                <x-app-element-label label="Descripción" />
+                <x-app-element-value value="{{ $application->applicable->description }}" />
+            </x-app-element>
+        </x-app-elements>
+    </x-card>
 </div>

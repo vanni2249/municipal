fd3fcddeccc68c01549f9699fb19d98e08c5b3ef
Geovanni Colon <vanni2249@gmail.com>
2026-02-15 01:57:30 -0400

    fix: update h1 component usage in event and news show pages for improved consistency and readability

--- resources/views/components/h1.blade.php
+++ resources/views/components/h1.blade.php
@@ -1,5 +1,5 @@
 @props(['value' => null])
-<h2 class="font-bold text-xl text-gray-900 line-clamp-1">
+<h2 {{ $attributes->merge(['class' => 'font-bold text-xl text-gray-900 line-clamp-1']) }}>
     {{ $value }}
     {{ $slot }}
 </h2>
--- resources/views/livewire/guest/events/show.blade.php
+++ resources/views/livewire/guest/events/show.blade.php
@@ -6,29 +6,29 @@
                     <span class="text-xs text-gray-700">
                         12 de Octubre de 2024
                     </span>
-                    <p class="font-bold text-gray-950">
+                    <x-h1 class="font-bold text-gray-950">
                         La ultima information sobre los eventos administrativos relacionados con el servicio
-                    </p>
-                    <ul class="text-gray-800">
-                        <li class="flex space-x-1 items-start text-sm">
-                            <x-icon icon="clock" height="16" width="16"
-                                class="text-gray-700 stroke-1 inline-block" />
-                            <span>
-                                Hora: 10:00 AM - 12:00 PM
-                            </span>
-                        </li>
-                        <li class="flex space-x-1 items-start text-sm">
-                            <x-icon icon="map-pin" height="16" width="16"
-                                class="text-gray-700 stroke-1 inline-block" />
-                            <span>
-                                Ubicación: Oficina de Servicios Municipales
-                            </span>
-                        </li>
-                    </ul>
+                    </x-h1>
                 </header>
                 <div class="flex justify-center mt-4 w-full bg-gray-100 rounded-md lg:p-4">
                     <img src="{{ asset('img/news/1.png') }}" alt="lake" class="object-cover rounded-md">
                 </div>
+                <ul class="text-gray-800">
+                    <li class="flex space-x-1 items-start text-sm">
+                        <x-icon icon="clock" height="16" width="16"
+                            class="text-gray-700 stroke-1 inline-block" />
+                        <span>
+                            Hora: 10:00 AM - 12:00 PM
+                        </span>
+                    </li>
+                    <li class="flex space-x-1 items-start text-sm">
+                        <x-icon icon="map-pin" height="16" width="16"
+                            class="text-gray-700 stroke-1 inline-block" />
+                        <span>
+                            Ubicación: Oficina de Servicios Municipales
+                        </span>
+                    </li>
+                </ul>
             </x-card>
         </div>
         <div class="col-span-full md:col-span-4 space-y-2">

d5d5d766cbcbacdcc0b9ff334e05a56eb682ce3e
Geovanni Colon <vanni2249@gmail.com>
2026-02-15 01:17:30 -0400

    feat: add new user icon SVG and update its styling for improved visual consistency

--- resources/views/components/icon.blade.php
+++ resources/views/components/icon.blade.php
@@ -345,16 +345,15 @@
         @break
 
         @case('user')
-            <svg xmlns="http://www.w3.org/2000/svg" width="{{ $width }}" height="{{ $height }}" viewBox="0 0 24 24" fill="none"
-                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                {{ $attributes->merge(['class' => 'icon icon-tabler icons-tabler-outline icon-tabler-user']) }}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="{{ $width }}" height="{{ $height }}" viewBox="0 0 24 24" fill="currentColor"
+                {{ $attributes->merge(['class' => 'icon icon-tabler icons-tabler-filled icon-tabler-user']) }}>
                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
-                <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+                <path d="M12 2a5 5 0 1 1 -5 5l.005 -.217a5 5 0 0 1 4.995 -4.783z" />
+                <path d="M14 14a5 5 0 0 1 5 5v1a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2v-1a5 5 0 0 1 5 -5h4z" />
             </svg>
         @break
 
-            {{-- Heroicons --}}
+        {{-- Heroicons --}}
         @case('arrow-right')
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
                 class="size-{{ $size }}">

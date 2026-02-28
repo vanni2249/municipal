7fc3e14a0547c5048d5c57682d16aeb9901046c3
Geovanni Colon <vanni2249@gmail.com>
2026-02-28 11:23:09 -0400

    refactor: Standardize widget component colors for consistency

--- resources/views/components/widget.blade.php
+++ resources/views/components/widget.blade.php
@@ -20,14 +20,14 @@
 @endphp
 
 <div
-    {{ $attributes->merge(['class' => 'border-' . $variants[$variant] . '-400 border-l-4 bg-white p-2 md:p-4 rounded-xl h-full hover:shadow-lg transition-shadow duration-300']) }}>
+    {{ $attributes->merge(['class' => 'border-gray-400 border-l-4 bg-white p-2 md:p-4 rounded-xl h-full hover:shadow-lg transition-shadow duration-300']) }}>
     <a href="{{ $attributes['href'] ?? '#' }}" class="flex flex-col h-full">
 
         <div class="flex flex-col h-full">
             <header class="flex justify-between  items-center">
                 <div class="flex items-center space-x-1">
-                    <div class="bg-{{ $variants[$variant] }}-200  p-1.5 rounded-full">
-                        <x-icon :icon="$icon" width="16" height="16" class="text-{{ $variants[$variant] }}-600" />
+                    <div class="bg-gray-200  p-1.5 rounded-full">
+                        <x-icon :icon="$icon" width="16" height="16" class="text-gray-600" />
                     </div>
                     <span class="text-xs text-gray-800 uppercase font-bold">
                         {{ $title }}

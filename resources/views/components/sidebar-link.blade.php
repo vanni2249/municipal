5b7322c5dcb31562cb79c2b5f2ab8cb86b718bc3
Geovanni Colon <vanni2249@gmail.com>
2026-03-01 09:14:01 -0400

    refactor: Enhance sidebar-link and widget components with group styling for improved hover effects

--- resources/views/components/sidebar-link.blade.php
+++ resources/views/components/sidebar-link.blade.php
@@ -1,19 +1,21 @@
     @props(['icon' => ''])
     <a {{ $attributes->merge([
-        'class' => 'blocktext-white p-2 tracking-widest rounded hover:bg-gray-800 flex items-center
-                    justify-between',
+        'class' => 'group block text-white p-2 tracking-widest rounded hover:bg-gray-800 transition-all flex items-center
+                            justify-between',
     ]) }}
         wire:navigate>
         <div class="flex items-center space-x-2">
-            <x-icon :icon="$icon" width="18" height="18" /> 
-            {{-- <x-icon icon="home" width="18" height="18" /> --}}
+            <x-icon :icon="$icon" width="18" height="18" />
             <span>
                 {{ $slot }}
             </span>
         </div>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-5 text-gray-200">
-            <path fill-rule="evenodd"
-                d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z"
-                clip-rule="evenodd" />
-        </svg>
+        <div class="group-hover:mr-0.5 transition-all">
+
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-5 text-gray-200">
+                <path fill-rule="evenodd"
+                    d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z"
+                    clip-rule="evenodd" />
+            </svg>
+        </div>
     </a>
--- resources/views/components/widget.blade.php
+++ resources/views/components/widget.blade.php
@@ -20,7 +20,7 @@
 @endphp
 
 <div
-    {{ $attributes->merge(['class' => 'border-gray-400 border-l-4 bg-white p-2 md:p-4 rounded-xl h-full hover:shadow-lg transition-shadow duration-300']) }}>
+    {{ $attributes->merge(['class' => 'group border-gray-400 border-l-4 bg-white p-2 md:p-4 rounded-xl h-full hover:shadow-lg transition-shadow duration-300']) }}>
     <a href="{{ $attributes['href'] ?? '#' }}" class="flex flex-col h-full">
 
         <div class="flex flex-col h-full">
@@ -33,7 +33,7 @@
                         {{ $title }}
                     </span>
                 </div>
-                <x-icon icon="arrow-up-right" class="w-5 h-5 text-gray-400" />
+                <x-icon icon="arrow-up-right" class="w-5 h-5 text-gray-400 group-hover:text-gray-600 transition-all duration-300 group-hover:mr-1" />
             </header>
             <span class="text-lg lg:text-xl text-gray-800 font-bold mt-1">{{ $value }}</span>
             <footer>
